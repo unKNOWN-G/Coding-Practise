7b9b808e763c63c1ca4401d98fadae6035d7fb33
Giri <karnatisaivenkatagiri@gmail.com>
2020-11-21 03:29:23 +0530

    Small Description About Arrays

---
+++ DSA/Data Structures/Arrays/Array Briefing.cpp
@@ -0,0 +1,14 @@
+/*
+ 21-11-2020 02:26
+ Arrays
+ 
+ An array is a collection of elements stored at continuos bytes of memory
+ It stores elements of same data type (In C)
+ Advantages 	: Finding an element in an array is easy (Time Complexity = O(1))
+ Disadvantages 	: Continous bits are needed, Insertion and deletion are difficult, Non Dynamic, No index out of Box
+ 
+ In C Language array length is Fixed once assigned.We cant expand or shrink it.
+ Because we cant be sure that if next byte is free or not
+ 
+ Using arrays for stacks is noob as we are allocating a large sting of continuos data to it even tough it might not be used
+*/
