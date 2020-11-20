f538cdc6ad62776a0ceb118a8fb16909912aa36b
Giri <karnatisaivenkatagiri@gmail.com>
2020-11-20 14:03:17 +0530

    Time Complexity Notatons

---
+++ DSA/Time Complexity notations.cpp
@@ -0,0 +1,13 @@
+/* 
+ 20-11-2020 13:50
+ Time Complexity notations
+ 
+ O(g(n))=f(n)  	 		=>implies there exist n0,c such that  f(n) <= c*g(n) ;n>=n0 
+ O(2*n2+6)=n2
+ 
+ Omega(g(n))=f(n)		=>implies there exist n0,c such that  f(n) >= c*g(n) ;n>=n0 
+ Omega(n2-10n)=n2
+ 
+ theta(g(n))=f(n)		=>implies there exist n0,c1,c2 such that  c1*g(n) <= f(n) <= c2*g(n) ;n>=n0 
+ theta(n2-5n)=n2
+*/
