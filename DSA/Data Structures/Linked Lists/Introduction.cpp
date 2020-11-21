b2360b414502885293bde07aa74021e06d3683ef
Giri <karnatisaivenkatagiri@gmail.com>
2020-11-22 02:48:29 +0530

    brief into about Linked Lists

---
+++ DSA/Data Structures/Linked Lists/Introduction.cpp
@@ -0,0 +1,16 @@
+/*
+ 21-11-2020 22:45
+ Introduction to linked-list Data Structure:
+ 
+ Why liked-list? Why not array?
+ 1)The size is fixed in an array but its not fixed in Linked list as its dynamically allocated
+ 2)Insertion are deletion operations are less expensive than in array
+ 
+ But at what cost?(Disavantages :p)
+ 1)Assessing a particular index element is so tough
+ 2)Pointer is a must
+ 3)Extra memory usage
+ 
+ Generally a Linked-List DataStructure contains a datatype variable and a pointer refering to next node
+*/
+ 
