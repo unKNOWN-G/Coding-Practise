a55bad86699c3ddfc342e6313fa953e7cd47a3d3
Giri <karnatisaivenkatagiri@gmail.com>
2020-12-13 11:58:16 +0530

    What are trees?

---
+++ DSA/Data Structures/Trees/Introduction.cpp
@@ -0,0 +1,10 @@
+/*
+ 10:35 13-12-2020
+ Introduction to trees Data Structures
+ 
+ Basically tree datastrcuture resembles tress
+ A special case of trees is Binary Tree where
+ Each Node has either 2 chidren or no children at all
+
+*/
+
