c60c7b034fbefd529d077dcf7fac23231cfc1cb8
Giri <karnatisaivenkatagiri@gmail.com>
2020-12-13 11:56:43 +0530

    Basic useful function in trees in c
    This reverts commit 6c7f2cb6afbf12c19609d9d7baa1d9220c311c5a.

--- DSA/Data Structures/Trees/Basic Functions.cpp
+++ DSA/Data Structures/Trees/Basic Functions.cpp
@@ -1,10 +1,10 @@
 /*
- 10:35 13-12-2020
- Introduction to trees Data Structures
+ 11:50 13-12-2020
+ Basic Functions
  
- Basically tree datastrcuture resembles tress
- A special case of trees is Binary Tree where
- Each Node has either 2 chidren or no children at all
+ These contains basic fnctions for creating a tree
+ Creating a node datastructure, using which we create a "createNode" Function for eay creation fo function
+ Then we write 2 more function create_leftNode and create_rightNode to create left and right nodes to the passed root node
 
 */
 
