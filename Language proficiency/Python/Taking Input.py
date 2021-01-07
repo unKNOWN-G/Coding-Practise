23b5ce0f4f32932f25028c978dd983eb1b26d638
Giri <karnatisaivenkatagiri@gmail.com>
2021-01-07 14:29:05 +0530

    Python Practice

--- Language proficiency/Python/Taking Input.py
+++
@@ -1,8 +0,0 @@
-"""
-16:04 06-01-2021
-Taking Input
-
-A command to take input as string -------- inout("Text to display")
-"""
-var = input("Enter Your age")
-print("Your Age is {}".format(var))
--- Language proficiency/Python/sWAP cASE.py
+++
@@ -1,8 +0,0 @@
-"""
- 15:07 06-01-2020
- Hackkerrank-> Python -> sWAP cASE
-
- The question is pretty simple invert the cases of given string
-
- The logic used is 
-"""
