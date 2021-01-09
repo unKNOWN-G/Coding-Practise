e6c3d44c462e067a6836ea97a73a50ce98032f5b
Giri <karnatisaivenkatagiri@gmail.com>
2021-01-09 09:38:55 +0530

    Arthimetic, Relational, Logical, Bitwise, Assignment, comma, conditional, sizeof

---
+++ Language proficiency/C++/Operators.cpp
@@ -0,0 +1,17 @@
+/*
+ 08:10 08-01-2021
+ Operators in C++
+ 
+ Arthimetic
+ 	a) Unary	-	++, --
+ 	b) Binary	-	+, -, /, *
+ Relational		-   >, <, >=, <=, ==, !=
+ Logical		-	&&, ||, !
+ Bitwise		-	&, |, <<, >>, ~(not), ^(xor)
+ Assignment		-	=, +=, -=,*=, /=
+ sizof Operator	
+ Comma Operator	-	,(Used as an operator and a Seperator. As an operator it can be used to tell the fn or statement before , to be excuted first
+ Conditional	-	Exp1?exp2:exp3 (example : a= 1>2?3:4
+
+*/
+ 
