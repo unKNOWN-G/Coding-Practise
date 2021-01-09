8203ff1b95140cd50107ec0f3b0b8afe4bcaf6cc
Giri <karnatisaivenkatagiri@gmail.com>
2021-01-09 09:39:37 +0530

    Switch case and Rules to implement switch

---
+++ Language proficiency/C++/Switch.cpp
@@ -0,0 +1,20 @@
+/*
+ 08:36 08-01-2021
+ Switch Statements
+ 
+ switch (n)
+ {
+     case 1: // code to be executed if n = 1;
+         break;
+     case 2: // code to be executed if n = 2;
+         break;
+     default: // code to be executed if n doesn't match any cases
+ }
+ 
+ Here  in place of 'n' it can even be a variable or a constant expression like 1>2?1:0
+ each case need ot have unique id
+ default is optional
+ Used to teminate sequence. If break isnt used next series of statents are exceuted too
+ Nesting switches is allowed
+
+*/
