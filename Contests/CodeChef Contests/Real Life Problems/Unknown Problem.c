a5b201d890e9fc6c3f030022576ab3b5ecf2fb48
Giri <karnatisaivenkatagiri@gmail.com>
2021-05-14 01:29:12 +0530

    Easy Identifyiyable one

---
+++ Contests/CodeChef Contests/Real Life Problems/Unknown Problem.c
@@ -0,0 +1,13 @@
+// Contest Code:RLIF2021 Problem Code:UNKNOWN1
+#include <stdio.h>
+
+int main(void) {
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%0.1f\n",(float)(a+b));
+	printf("%0.1f\n",(float)(a-b));
+	printf("%0.1f\n",(float)(a*b));
+	printf("%0.1f\n",((float)(a)/b));
+	return 0;
+}
+
