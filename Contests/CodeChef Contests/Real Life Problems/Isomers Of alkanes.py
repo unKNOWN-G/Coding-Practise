0df3c44001b0d92af19be99a2c713bcb1ac541df
Giri <karnatisaivenkatagiri@gmail.com>
2021-05-14 01:29:49 +0530

    Used Available data of Isomers on Internet to create a list

---
+++ Contests/CodeChef Contests/Real Life Problems/Isomers Of alkanes.py
@@ -0,0 +1,8 @@
+# Contest Code:RLIF2021 Problem Code:ISOMER
+
+a = [0,1,1,1,2,3,5,9,18,35,75,159,355,802,1858,4347,10359,24894,60523,148284,366319]
+
+test_num  = int(input())
+for i in range(test_num):
+    x = int(input())
+    print(a[x])
