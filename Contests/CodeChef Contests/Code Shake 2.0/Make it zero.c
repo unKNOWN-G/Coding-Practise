b57e94c464d5139072ed05d72ed55682fa72fdab
Giri <karnatisaivenkatagiri@gmail.com>
2021-05-18 09:58:36 +0530

    Wasnt clear eneough

---
+++ Contests/CodeChef Contests/Code Shake 2.0/Make it zero.c
@@ -0,0 +1,15 @@
+//Problem: MAKEIT Contest: SHAK2021 
+
+#include <stdio.h>
+
+int main(void) {
+	int test_Cases;
+	scanf("%d",&test_Cases);
+	for(int i=0;i<test_Cases;i++)
+	{
+	    printf("YES\n");
+	}
+	return 0;
+}
+
+
